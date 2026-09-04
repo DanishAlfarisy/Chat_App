d7699e3852c0d60ec5d128717f74c1a580861140
Danish Alfarisy <danishalfarisy10@gmail.com>
2026-09-04 14:11:08 +0700

    Adding logic for sending message

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from 'react';
+import './App.css';
 
 function App() {
     const [messages, setMessages] = useState([]);
+    const [newMessage, setNewMessage] = useState('');
 
     useEffect(() => {
         fetch('http://localhost:3000/messages/1/2')
@@ -14,15 +16,66 @@ function App() {
             });
     }, []);
 
-    return (
-        <div>
-            <h1>Simple Chat</h1>
+    const sendMessage = () => {
+    if (newMessage.trim() === '') {
+        return;
+    }
 
-            {messages.map(message => (
-                <p key={message.id}>
-                    {message.message}
-                </p>
-            ))}
+    fetch('http://localhost:3000/messages/1/2', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            message: newMessage
+        })
+    })
+        .then(response => response.json())
+        .then(() => {
+            setNewMessage('');
+
+            fetch('http://localhost:3000/messages/1/2')
+                .then(response => response.json())
+                .then(data => {
+                    setMessages(data);
+                });
+        });
+};
+
+    return (
+        <div className="chat-container">
+
+            <div className="chat-header">
+                <h2>Chat App</h2>
+                <p>Daffa</p>
+            </div>
+
+            <div className="chat-messages">
+                {messages.map(message => (
+                    <div
+                        key={message.id}
+                        className={
+                            message.sender_id === 1
+                                ? 'message sent'
+                                : 'message received'
+                        }
+                    >
+                        <p>{message.message}</p>
+                    </div>
+                ))}
+            </div>
+
+            <div className="chat-input">
+                <input
+                    type="text"
+                    placeholder="Ketik pesan..."
+                     value={newMessage}
+                    onChange={(e) => setNewMessage(e.target.value)}
+                />
+
+                <button onClick={sendMessage}>Send</button>
+            </div>
+
         </div>
     );
 }
